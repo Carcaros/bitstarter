2e9fabcd89614b5b553e2763f7cec8ec108d217a
Paul <paul@lightbox.dnsd.info>
2013-08-12 22:34:03 +0100

    Returning contents of index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  var fileBuf = fs.readFile("index.html");
+  response.send(fileBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
